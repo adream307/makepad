ba09f7dd8b7df390c53f03e7b83d36f3b8504490
chenyefu <chenyefu1@huawei.com>
2024-08-05 17:08:15 +0800

    hilog
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- platform/src/log.rs
+++ platform/src/log.rs
@@ -91,8 +91,13 @@ pub fn log_with_level(file_name:&str, line_start:u32, column_start:u32, line_end
        #[cfg(target_env="ohos")]
        {
             let msg = format!("{}:{}:{} - {}\0", file_name, line_start, column_start, message);
-            unsafe {hilog_sys::OH_LOG_Print(hilog_sys::LogType::LOG_APP,
-                hilog_sys::LogLevel::LOG_INFO, 0,c"makepad-ohos".as_ptr(), msg.as_ptr())};
+            let hilevel:hilog_sys::LogLevel = match level {
+                LogLevel::Warning => {hilog_sys::LogLevel::LOG_WARN}
+                LogLevel::Error => {hilog_sys::LogLevel::LOG_ERROR}
+                LogLevel::Log => {hilog_sys::LogLevel::LOG_INFO}
+                _=> {hilog_sys::LogLevel::LOG_INFO}
+            };
+            unsafe {hilog_sys::OH_LOG_Print(hilog_sys::LogType::LOG_APP,hilevel, 0,c"makepad-ohos".as_ptr(), msg.as_ptr())};
        }
     }
     else{
