48b0306f2445c70fb5b4f5bdd072ea95be361bd2
chenyefu <chenyefu1@huawei.com>
2024-08-08 18:57:38 +0800

    send msg
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- platform/src/os/linux/open_harmony/open_harmony.rs
+++ platform/src/os/linux/open_harmony/open_harmony.rs
@@ -107,9 +107,14 @@ thread_local! {
 
 fn send_from_ohos_message(message: FromOhosMessage) {
     OHOS_MSG_TX.with(|tx| {
-        let mut tx = tx.borrow_mut();
-        tx.as_mut().unwrap().send(message).unwrap();
-    })
+        loop{
+            let mut tx = tx.borrow_mut();
+            if !tx.is_none(){
+                tx.as_mut().unwrap().send(message).unwrap();
+                break;
+            }
+        }
+    });
 }
 
 #[no_mangle]
