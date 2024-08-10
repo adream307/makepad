d8defadeb09b336afe94b96251d71a98e789eacc
chenyefu <chenyefu1@huawei.com>
2024-08-10 11:58:41 +0800

    fix
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- platform/src/os/linux/open_harmony/mod.rs
+++ platform/src/os/linux/open_harmony/mod.rs
@@ -1,2 +1,4 @@
 pub mod oh_media;
+pub mod oh_sys;
+pub mod oh_callbacks;
 pub mod open_harmony;
---
+++ platform/src/os/linux/open_harmony/oh_sys.rs
@@ -0,0 +1,25 @@
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+
+#[repr(C)]
+pub struct OH_NativeVSync {
+    _unused: [u8; 0],
+}
+
+#[link(name = "ace_napi.z")]
+#[link(name = "ace_ndk.z")]
+#[link(name = "hilog_ndk.z")]
+#[link(name = "native_window")]
+#[link(name = "native_vsync")]
+extern "C" {
+    pub fn OH_NativeVSync_Create(name:* const::core::ffi::c_char, length: ::core::ffi::c_uint) -> * mut OH_NativeVSync;
+    pub fn OH_NativeVSync_Destroy(nativeVsync: *mut OH_NativeVSync) -> ::core::ffi::c_void;
+    pub fn OH_NativeVSync_RequestFrame(
+        nativeVsync: *mut OH_NativeVSync,
+        callback : extern "C" fn (timestamp: ::core::ffi::c_longlong, data: *mut ::core::ffi::c_void),
+        data: *mut ::core::ffi::c_void) -> ::core::ffi::c_int;
+    pub fn OH_NativeVSync_GetPeriod (
+        nativeVsync:  *mut OH_NativeVSync,
+        period: * mut ::core::ffi::c_longlong) -> ::core::ffi::c_int;
+}
