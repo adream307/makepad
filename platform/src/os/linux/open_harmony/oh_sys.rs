bb4b416d324835a433bff0bb1a0813c30e2f3136
chenyefu <chenyefu1@huawei.com>
2024-08-19 17:22:44 +0800

    add ffi for libuv
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- platform/src/os/linux/open_harmony/oh_sys.rs
+++ platform/src/os/linux/open_harmony/oh_sys.rs
@@ -1,6 +1,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+use std::ffi::*;
+use napi_ohos::sys::*;
+
 #[repr(C)]
 pub struct OH_NativeVSync {
     _unused: [u8; 0],
@@ -13,18 +16,58 @@ pub struct OH_NativeVSync {
 #[link(name = "native_vsync")]
 extern "C" {
     pub fn OH_NativeVSync_Create(
-        name: *const ::core::ffi::c_char,
-        length: ::core::ffi::c_uint,
+        name: *const c_char,
+        length: c_uint,
     ) -> *mut OH_NativeVSync;
-    pub fn OH_NativeVSync_Destroy(nativeVsync: *mut OH_NativeVSync) -> ::core::ffi::c_void;
+    pub fn OH_NativeVSync_Destroy(nativeVsync: *mut OH_NativeVSync) -> c_void;
     pub fn OH_NativeVSync_RequestFrame(
         nativeVsync: *mut OH_NativeVSync,
-        callback: extern "C" fn(timestamp: ::core::ffi::c_longlong, data: *mut ::core::ffi::c_void),
-        data: *mut ::core::ffi::c_void,
-    ) -> ::core::ffi::c_int;
+        callback: extern "C" fn(timestamp: c_longlong, data: *mut c_void),
+        data: *mut c_void,
+    ) -> c_int;
     pub fn OH_NativeVSync_GetPeriod(
         nativeVsync: *mut OH_NativeVSync,
-        period: *mut ::core::ffi::c_longlong,
-    ) -> ::core::ffi::c_int;
+        period: *mut c_longlong,
+    ) -> c_int;
 
+}
+
+
+// libuv
+
+pub type uv_loop_t = napi_ohos::sys::uv_loop_s;
+pub type uv_req_type = u32;
+pub type uv_work_t = uv_work_s;
+pub type uv_work_cb = Option<unsafe extern "C" fn(req: *mut uv_work_t)>;
+pub type uv_after_work_cb = Option<unsafe extern "C" fn(req: *mut uv_work_t, status: c_int)>;
+
+#[repr(C)]
+pub struct uv_work_s {
+    pub data: *mut c_void,
+    pub type_: uv_req_type,
+    pub reserved: [*mut c_void; 6],
+    pub loop_: *mut uv_loop_t,
+    pub work_cb: uv_work_cb,
+    pub after_work_cb: uv_after_work_cb,
+    pub work_req: uv__work,
+}
+
+#[repr(C)]
+pub struct uv__work {
+    pub work: Option<unsafe extern "C" fn(w: *mut uv__work)>,
+    pub done: Option<unsafe extern "C" fn(w: *mut uv__work, status: c_int)>,
+    pub loop_: *mut uv_loop_s,
+    pub wq: [*mut c_void; 2],
+}
+
+
+
+#[link(name = "uv")]
+extern "C" {
+    pub fn uv_queue_work(
+        loop_: *mut uv_loop_t,
+        req: *mut uv_work_t,
+        work_cb: uv_work_cb,
+        after_work_cb: uv_after_work_cb
+    ) -> c_int;
 }
