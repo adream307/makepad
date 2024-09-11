ec895aaebe772b069100b312c264dd14dbc94993
chenyefu <chenyefu1@huawei.com>
2024-09-11 09:03:16 +0800

    oh media
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- platform/src/os/linux/open_harmony/oh_media.rs
+++ platform/src/os/linux/open_harmony/oh_media.rs
@@ -1,24 +1,28 @@
+
 use {
     //std::sync::{Arc, Mutex},
     crate::{
+        cx::Cx,
         //event::Event,
         //thread::SignalToUI,
         audio::*,
-        cx::Cx,
-        media_api::CxMediaApi,
         midi::*,
         video::*,
-    },
+        media_api::CxMediaApi,
+    }
 };
 
 #[derive(Clone)]
-pub struct OsMidiOutput {}
-
-impl OsMidiOutput {
-    pub fn send(&self, _port_id: Option<MidiPortId>, _data: MidiData) {}
+pub struct OsMidiOutput {
 }
 
-pub struct OsMidiInput {}
+impl OsMidiOutput {
+    pub fn send(&self, _port_id: Option<MidiPortId>, _data: MidiData) {
+    }
+}
+
+pub struct OsMidiInput {
+}
 
 impl OsMidiInput {
     pub fn receive(&mut self) -> Option<(MidiPortId, MidiData)> {
@@ -26,41 +30,58 @@ impl OsMidiInput {
     }
 }
 
+
 #[derive(Default)]
-pub struct CxOpenHarmonyMedia {}
-
-impl Cx {
-    pub(crate) fn handle_media_signals(&mut self /*, to_java: &AndroidToJava*/) {}
-
-    pub fn reinitialise_media(&mut self) {}
+pub struct CxOpenHarmonyMedia {
 }
 
-impl CxOpenHarmonyMedia {}
+impl Cx {
+    pub (crate) fn handle_media_signals(&mut self/*, to_java: &AndroidToJava*/) {
+    }
+    
+    pub fn reinitialise_media(&mut self){
+    }
+}
+
+impl CxOpenHarmonyMedia {
+}
 
 impl CxMediaApi for Cx {
+    
     fn midi_input(&mut self) -> MidiInput {
         MidiInput(Some(OsMidiInput {}))
     }
-
+    
     fn midi_output(&mut self) -> MidiOutput {
         MidiOutput(Some(OsMidiOutput {}))
     }
-
+    
     fn midi_reset(&mut self) {}
-
-    fn use_midi_inputs(&mut self, _ports: &[MidiPortId]) {}
-
-    fn use_midi_outputs(&mut self, _ports: &[MidiPortId]) {}
-
-    fn use_audio_inputs(&mut self, _devices: &[AudioDeviceId]) {}
-
-    fn use_audio_outputs(&mut self, _devices: &[AudioDeviceId]) {}
-
-    fn audio_output_box(&mut self, _index: usize, _f: AudioOutputFn) {}
-
-    fn audio_input_box(&mut self, _index: usize, _f: AudioInputFn) {}
-
-    fn video_input_box(&mut self, _index: usize, _f: VideoInputFn) {}
-
-    fn use_video_input(&mut self, _inputs: &[(VideoInputId, VideoFormatId)]) {}
+    
+    fn use_midi_inputs(&mut self, _ports: &[MidiPortId]) {
+    }
+    
+    fn use_midi_outputs(&mut self, _ports: &[MidiPortId]) {
+    }
+    
+    fn use_audio_inputs(&mut self, _devices: &[AudioDeviceId]) {
+    }
+    
+    fn use_audio_outputs(&mut self, _devices: &[AudioDeviceId]) {
+    }
+    
+    fn audio_output_box(&mut self, _index: usize, _f: AudioOutputFn) {
+    }
+    
+    fn audio_input_box(&mut self, _index: usize, _f: AudioInputFn) {
+    }
+    
+    fn video_input_box(&mut self, _index: usize, _f: VideoInputFn) {
+    }
+    
+    fn use_video_input(&mut self, _inputs: &[(VideoInputId, VideoFormatId)]) {
+    }
 }
+
+
+
