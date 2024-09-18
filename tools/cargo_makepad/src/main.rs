289b09bd88dc383d058b9bb74864c4fb7dd06107
chenyefu <chenyefu1@huawei.com>
2024-09-18 17:14:29 +0800

    build for ohos
    Signed-off-by: chenyefu <chenyefu1@huawei.com>

--- tools/cargo_makepad/src/main.rs
+++ tools/cargo_makepad/src/main.rs
@@ -82,8 +82,9 @@ println!("    wasm [options] run <cargo args>              Build and run a wasm 
     println!("Open Harmony commands:");
     println!();
     println!("    ohos [options] install-toolchain             Install the toolchain needed with rustup");
-    println!("    ohos [options] build <cargo args>            Create an DevEco project for Open Harmony OS and build it");
-    println!("       --project-path=\"path\"                   The path of the DevEco Project");
+    println!("    ohos [options] build <cargo args>            Build an DevEco project for Open Harmony OS");
+    println!("       --project-path=\"project_path\"           The path of the DevEco Project");
+    println!("       --deveco-home=\"deveco_path\"             The path of DevEco program");
     println!();
     println!("Linux commands:");
     println!();
--- tools/cargo_makepad/src/open_harmony/mod.rs
+++ tools/cargo_makepad/src/open_harmony/mod.rs
@@ -6,7 +6,6 @@ use compile::*;
 pub enum HostOs {
     WindowsX64,
     MacosX64,
-    MacosAarch64,
     LinuxX64,
     Unsupported
 }
@@ -14,29 +13,52 @@ pub enum HostOs {
 #[allow(non_camel_case_types)]
 pub enum OpenHarmonyTarget {
     aarch64,
-    x86_64,
-    armv7
 }
 
 impl OpenHarmonyTarget {
     fn toolchain(&self) ->&'static str{
         match self {
-            Self::aarch64 => "aarch64-unknown-linux-ohos",
-            Self::armv7 => "armv7-unknown-linux-ohos",
-            Self::x86_64 => "x86_64-unknown-linux-ohos"
-
+            Self::aarch64 => "aarch64-unknown-linux-ohos"
         }
     }
 }
 
 pub fn handle_open_harmony(args: &[String]) -> Result<(), String> {
+    #[allow(unused)]
+    let mut host_os = HostOs::Unsupported;
+    #[cfg(all(target_os = "windows", target_arch = "x86_64"))] let mut host_os = HostOs::WindowsX64;
+    #[cfg(all(target_os = "macos", target_arch = "x86_64"))] let mut host_os = HostOs::MacosX64;
+    #[cfg(all(target_os = "linux", target_arch = "x86_64"))] let mut host_os = HostOs::LinuxX64;
+    let targets = vec![OpenHarmonyTarget::aarch64];
+    let mut project_path = None;
+    let mut deveco_home = None;
+
+    for i in 0..args.len() {
+        let v = &args[i];
+        if let Some(opt) = v.strip_prefix("--project-path") {
+            project_path = Some(opt.to_string())
+        }
+        else if let Some(opt) = v.strip_prefix("-deveco-home") {
+            deveco_home = Some(opt.to_string());
+        }
+
+    }
+
+    if deveco_home.is_none() {
+        if let Ok(v) = std::env::var("DEVECO_HOME") {
+            deveco_home = Some(v);
+        }
+    }
+
     if args.len() < 1 {
         return Err(format!("not enough args"))
     }
     match args[0].as_ref() {
         "toolchain-install" | "install-toolchain" => {
-            let toolchains = vec![OpenHarmonyTarget::aarch64];
-            sdk::rustup_toolchain_install(&toolchains)
+            sdk::rustup_toolchain_install(&targets)
+        }
+        "build" => {
+            compile::build(&deveco_home, &project_path)
         }
         _ => Err(format!("{} is not a valid command or option", args[0]))
 
